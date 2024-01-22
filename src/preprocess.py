af01a23e06e3aca0f336d72114e2a80d8d010886
Raoul Grouls <Raoul.Grouls@han.nl>
2024-01-22 22:32:58 +0100

    notebooks

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,10 +1,12 @@
-from datetime import datetime
-from pathlib import Path
-from loguru import logger
 import re
 import sys
+from datetime import datetime
+from pathlib import Path
+
 import pandas as pd
-from src.settings import Regexes, Folders
+from loguru import logger
+
+from src.settings import Folders, Regexes
 
 logger.remove()
 logger.add("logs/logfile.log", rotation="1 MB", level="DEBUG")
@@ -12,11 +14,10 @@ logger.add(sys.stderr, level="INFO")
 
 
 if __name__ == "__main__":
-
     folders = Folders(
         raw=Path("data/raw"),
         processed=Path("data/processed"),
-        datafile=Path("_chat.txt")
+        datafile=Path("_chat.txt"),
     )
     regexes = Regexes()
 
@@ -31,12 +32,14 @@ if __name__ == "__main__":
                 msg = re.sub(regexes.clear, "", line)
                 author = re.search(regexes.author, line)
                 if author:
-                    author = author.group(0)
-                records.append((timestamp, author, msg))
+                    name = author.group(0)
+                else:
+                    name = "Unknown"
+                records.append((timestamp, name, msg))
             else:
                 appended.append(timestamp)
                 msg = msg + re.sub(regexes.clear, "", line)
-                records[-1] = (timestamp, author, msg)
+                records[-1] = (timestamp, name, msg)
 
     logger.info(f"Found {len(records)} records")
     logger.info(f"Appended {len(appended)} records")
@@ -48,12 +51,3 @@ if __name__ == "__main__":
     logger.info(f"Writing to {outfile}")
     df.to_csv(outfile, index=False)
     logger.success("Done!")
-
-
-
-
-
-
-
-
-
--- src/settings.py
+++ src/settings.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+
 @dataclass
 class Regexes:
     timestamp = r"(?<=\[)\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}(?=\])"
     author = r"(?<=\]\s)(.*?)(?=:)"
     clear = r"\[\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}\]\s[~a-zA-Z\s]+:"
+
 
 @dataclass
 class Folders:
