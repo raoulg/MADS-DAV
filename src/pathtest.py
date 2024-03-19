25f3a72d878cd4458e4513ec1afa0c3509209559
Raoul Grouls <Raoul.Grouls@han.nl>
2024-03-19 12:32:12 +0100

    better spacing in pathtest

--- src/pathtest.py
+++ src/pathtest.py
@@ -10,10 +10,13 @@ for i, p in enumerate(sys.path):
 
 logger.info("===== PYTHONPATH =====")
 pythonpath = str(os.environ.get("PYTHONPATH"))
+
 if not pythonpath:
     pythonpath = "None"
+
 assert isinstance(pythonpath, str)
 logger.info(f"Length of PYTHONPATH: {len(pythonpath.split(os.pathsep))}")
+
 for i, p in enumerate(pythonpath.split(os.pathsep)):
     logger.info(f"PYTHONPATH[{i}]: {p}")
 
@@ -22,6 +25,7 @@ path = str(os.environ.get("PATH"))
 if not path:
     path = "None"
 assert isinstance(path, str)
+
 logger.info(f"Length of PATH: {len(path.split(os.pathsep))}")
 for i, p in enumerate(path.split(os.pathsep)):
     logger.info(f"PATH[{i}]: {p}")
